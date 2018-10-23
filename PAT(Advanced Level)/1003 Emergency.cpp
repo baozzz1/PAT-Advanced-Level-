341067cbabd47ad8bdfd3f3d0530d2928b3c297d
鲍建军 <baojianjun786@outlook.com>
2018-10-23 16:56:51 +0800

    To Question 3.

---
+++ PAT(Advanced Level)/1003 Emergency.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+//Graph
+int main() {
+	int N, M, C1, C2;
+	cin >> N >> M >> C1 >> C2;
+	vector<int> rescueTeams(N);
+	for (int i = 0; i < N; i++)
+		cin >> rescueTeams[i];
+	int c1, c2, L;
+	for (int i = 0; i < M; i++) {
+		cin >> c1 >> c2 >> L;
+
+	}
+	return 0;
+}
