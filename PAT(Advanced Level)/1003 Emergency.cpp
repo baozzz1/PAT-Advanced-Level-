71a965d67a28fc3ed17ed46a1ef1d93d5c061e81
baozzz1 <baojianjun786@outlook.com>
2019-01-03 23:30:40 +0800

    update

--- PAT(Advanced Level)/1003 Emergency.cpp
+++ PAT(Advanced Level)/1003 Emergency.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<set>
 using namespace std;
+
+class Graph {
+	typedef vector<vector<int>> bag;
+public:
+	Graph(int V);
+
+
+private:
+	int E, V;
+	bag adj;
+};
+
+Graph::Graph(int V) {
+	if (V < 0)
+		return;
+	this->V = V;
+	this->E = 0;
+	adj = bag(V);
+	for (int v = 0; v < V; v++)
+		adj[v] = vector<int>();
+}
 
 //Graph
 int main() {
