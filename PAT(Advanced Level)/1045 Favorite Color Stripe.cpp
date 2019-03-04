516ae09889bd79546492ce37fca0b57002c53d58
鲍建军 <baojianjun786@outlook.com>
2019-03-04 12:45:50 +0800

    update

---
+++ PAT(Advanced Level)/1045 Favorite Color Stripe.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//#include<cstdio>
+//#include<vector>
+//#include<unordered_map>
+//#include<algorithm>
+//using namespace std;
+//
+//int main(){
+//	int N; cin >> N;
+//	int M; cin >> M;
+//	int favorColor;
+//	for (int i = 0; i < M; i++) {
+//		cin >> favorColor;
+//	}
+//	int L; cin >> L;
+//	for (int i = 0; i < L; i++) {
+//
+//	}
+//
+//	return 0;
+//
+//}
